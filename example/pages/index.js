4e2193755d894533d1b1cc1283069f2fdf4d5ab0
Niels Grafen <nielsgrafen@gmail.com>
2022-02-28 07:30:40 -0500

    Add alt prop to example image

--- example/pages/index.js
+++ example/pages/index.js
@@ -31,6 +31,7 @@ export default function Home() {
             id="test_image"
             objectFit="cover"
             priority={true}
+            alt={"test_image"}
           />
         </div>
         <ExportedImage
