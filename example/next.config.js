9e1e2569a8c99ea39875fb98ba3a0eb7a638d5e0
Niels <47059882+Niels-IO@users.noreply.github.com>
2022-04-16 21:56:05 +0200

    Update keys in next.config.js to the correct level

--- example/next.config.js
+++ example/next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   images: {
     loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     nextImageExportOptimizer: {
       imageFolderPath: "public/images",
       exportFolderPath: "out",
-      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       quality: 75,
     },
   },
