6de159832e6a4d5feb1ffeb2c66f33a55ef1b2e9
Niels <47059882+Niels-IO@users.noreply.github.com>
2022-10-18 11:42:03 +0200

    Add Types for future image

--- example/pages/typescript.tsx
+++ example/pages/typescript.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
-import ExportedImage from "../localTestComponent/ExportedImage";
+import ExportedImageFuture_Local from "../localTestComponent/ExportedImageFuture";
+import ExportedImage_Local from "../localTestComponent/ExportedImage";
+import ExportedImage from "next-image-export-optimizer";
+import ExportedImageFuture from "next-image-export-optimizer/future/ExportedImage";
+
 import React from "react";
 import Image from "next/image";
 import testPictureStatic from "../public/chris-zhang-Jq8-3Bmh1pQ-unsplash_static.jpg";
@@ -26,11 +30,27 @@ export default function Home() {
             marginBottom: "3rem",
           }}
         >
-          <ExportedImage
+          <ExportedImageFuture
             src="images/chris-zhang-Jq8-3Bmh1pQ-unsplash.jpg"
-            layout="fill"
-            id="test_image"
-            objectFit="cover"
+            id="test_image_future"
+            fill
+            priority={true}
+            alt={"test_image"}
+            useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}
+          />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            width: "50%",
+            height: "200px",
+            marginBottom: "3rem",
+          }}
+        >
+          <ExportedImageFuture_Local
+            src="images/chris-zhang-Jq8-3Bmh1pQ-unsplash.jpg"
+            id="test_image_future_local"
+            fill
             priority={true}
             alt={"test_image"}
             useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}
@@ -48,6 +68,21 @@ export default function Home() {
             src={testPictureStatic}
             alt="test_image_static"
             id="test_image_static"
+            layout="responsive"
+            useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}
+          />
+        </div>
+        <div
+          style={{
+            position: "relative",
+            marginBottom: "3rem",
+            width: "100%",
+          }}
+        >
+          <ExportedImage_Local
+            src={testPictureStatic}
+            alt="test_image_static"
+            id="test_image_static_local"
             layout="responsive"
             useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}
           />
