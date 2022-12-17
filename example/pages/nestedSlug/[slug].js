7b27818017428964f493906da1f70f9ddcc2872f
Niels <47059882+Niels-IO@users.noreply.github.com>
2022-12-17 19:37:05 +0100

    Fix image fallback on nested slug

--- example/pages/nestedSlug/[slug].js
+++ example/pages/nestedSlug/[slug].js
@@ -1,4 +1,5 @@
 import React from "react";
+import ExportedImage from "../../localTestComponent/ExportedImage";
 import ExportedImageLegacy from "../../localTestComponent/ExportedImageLegacy";
 import testPictureStatic from "../../public/chris-zhang-Jq8-3Bmh1pQ-unsplash_static.jpg";
 
@@ -20,6 +21,40 @@ function Slug() {
           alt="test_image_static"
           id="test_image_static"
           layout="responsive"
+        />
+      </div>
+      <h2>Optimized example (fill)</h2>
+      <div
+        style={{
+          position: "relative",
+          width: "50%",
+          height: "200px",
+          marginBottom: "3rem",
+        }}
+      >
+        <ExportedImage
+          src="images/chris-zhang-Jq8-3Bmh1pQ-unsplash.jpg"
+          id="test_image_future_fill"
+          alt={"test_image"}
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <h2>Optimized example (fill & static import)</h2>
+      <div
+        style={{
+          position: "relative",
+          width: "50%",
+          height: "200px",
+          marginBottom: "3rem",
+        }}
+      >
+        <ExportedImage
+          src={testPictureStatic}
+          id="test_image_future_static_fill"
+          alt={"test_image"}
+          fill
+          style={{ objectFit: "cover" }}
         />
       </div>
     </div>
