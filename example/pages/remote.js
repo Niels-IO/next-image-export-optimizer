e6dd3ae746141f0180e0c2f799db10370a228613
Niels <47059882+Niels-IO@users.noreply.github.com>
2023-01-18 16:22:55 +0100

    Add example code for remote images

---
+++ example/pages/remote.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next-Image-Export-Optimizer</title>
+        <meta
+          name="description"
+          content="Example of next-image-export-optimizer"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Next-Image-Export-Optimizer</h1>
+        <h2>Optimized example - Remote</h2>
+        <div
+          style={{
+            position: "relative",
+            width: "50%",
+            height: "200px",
+            marginBottom: "3rem",
+          }}
+        >
+          <Image
+            src="https://reactapp.dev/images/nextImageExportOptimizer/BG_HERO-opt-2048.WEBP"
+            fill
+            unoptimized
+            loader={({ src }) => {
+              return src;
+            }}
+            id="test_image"
+            style={{ objectFit: "cover" }}
+            priority
+            alt={"test_image"}
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
