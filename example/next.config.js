4c6dcf5d7661eba659e1eb7aa656bf4a4f447569
Niels <47059882+Niels-IO@users.noreply.github.com>
2024-05-25 09:12:47 +0200

    chore: Update package.json version to 1.13.0, update packages

--- example/next.config.js
+++ example/next.config.js
@@ -13,6 +13,5 @@ module.exports = {
     nextImageExportOptimizer_quality: "75",
     nextImageExportOptimizer_storePicturesInWEBP: "true",
     nextImageExportOptimizer_generateAndUseBlurImages: "true",
-    nextImageExportOptimizer_remoteImageCacheTTL: "0",
   },
 };
