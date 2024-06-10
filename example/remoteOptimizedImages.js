6e44ee30f7c457f1b0c5c587510205e760b8ba5c
Niels <47059882+Niels-IO@users.noreply.github.com>
2024-06-10 20:24:54 +0200

    Enable setting of custom headers in remoteOptimizedImages.js

--- example/remoteOptimizedImages.js
+++ example/remoteOptimizedImages.js
@@ -1,6 +1,10 @@
 module.exports = [
   "https://reactapp.dev/images/nextImageExportOptimizer/christopher-gower-m_HRfLhgABo-unsplash-opt-2048.WEBP",
   "https://reactapp.dev/images/nextImageExportOptimizer/christopher-gower-m_HRfLhgABo-unsplash-opt-2048.WEBP?ref=next-image-export-optimizer",
+  {
+    url: "https://reactapp.dev/images/nextImageExportOptimizer/christopher-gower-m_HRfLhgABo-unsplash-opt-2048.WEBP?ref=next-image-export-optimizer",
+    headers: { "User-Agent": "MyTestApp" },
+  },
   // 'https://example.com/image1.jpg',
   // 'https://example.com/image2.jpg',
   // 'https://example.com/image3.jpg',
--- src/utils/getRemoteImageURLs.ts
+++ src/utils/getRemoteImageURLs.ts
@@ -3,11 +3,16 @@ import fs from "fs";
 
 const urlToFilename = require("./urlToFilename");
 
+export type RemoteImage = {
+  url: string;
+  headers: HeadersInit;
+};
+
 export async function getRemoteImageURLs(
   nextConfigFolder: string,
   folderPathForRemoteImages: string
 ) {
-  let remoteImageURLs: string[] = [];
+  let remoteImageURLs: RemoteImage[] = [];
   const remoteImagesFilePath = path.join(
     nextConfigFolder,
     "remoteOptimizedImages.js"
@@ -16,10 +21,22 @@ export async function getRemoteImageURLs(
     const remoteOptimizedImages = await require(remoteImagesFilePath);
 
     remoteImageURLs = remoteOptimizedImages;
+
+    // go through the remote images and check if the entry is an object or a string
+    // if it's a string, convert it to an object
+
+    remoteImageURLs = remoteImageURLs.map((entry: any) => {
+      if (typeof entry === "string") {
+        return {
+          url: entry,
+        };
+      }
+      return entry;
+    });
   }
   // Create the filenames for the remote images
-  const remoteImageFilenames = remoteImageURLs.map((url: string) => {
-    const encodedURL = urlToFilename(url);
+  const remoteImageFilenames = remoteImageURLs.map((urlObj: RemoteImage) => {
+    const encodedURL = urlToFilename(urlObj.url);
 
     const filename = path.join(folderPathForRemoteImages, encodedURL);
 
